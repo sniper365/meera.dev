5bb5a392b14e74319cf8714c2658fc49b9b341eb
Meer Bahadin <59086176+meerbahadin@users.noreply.github.com>
2022-03-17 15:14:43 +0300

    Update Timeline.jsx

--- components/pages/index/Timeline.jsx
+++ components/pages/index/Timeline.jsx
@@ -1,9 +1,25 @@
-import { Heading, SlideFade, List, ListItem, ListIcon } from "@chakra-ui/react";
+import { Heading, SlideFade, List, ListItem, ListIcon, Link } from "@chakra-ui/react";
 import { MdCheckCircle } from "react-icons/md";
 
 const Timeline = () => {
   return (
     <SlideFade in={true} offsetY={80} delay={0.2}>
+    <Heading
+        as="h1"
+        fontSize={{ base: "24px", md: "30px", lg: "36px" }}
+        mb={3}
+      >
+        Timeline
+      </Heading>
+      <Heading mt={10} mb={5} as="h2" size="md">
+        2022
+      </Heading>
+      <List spacing={18} fontSize="18">
+        <ListItem>
+          <ListIcon as={MdCheckCircle} color="green.500" />
+          Front-end developer at <Link href="netspot.solutions">Netspot Sulotions</Link>
+        </ListItem>
+      </List>
       <Heading
         as="h1"
         fontSize={{ base: "24px", md: "30px", lg: "36px" }}
